027468aa7924073211d6d617579514540f697caf
Bryan Personal <bryanlohjw97@gmail.com>
2023-03-12 18:06:20 +0800

    fix: type error

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -15,9 +15,15 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  getAll: publicProcedure.query(
+    (
+      {
+        // ctx
+      }
+    ) => {
+      // return ctx.prisma.example.findMany();
+    }
+  ),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
