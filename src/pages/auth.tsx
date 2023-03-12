a95d8f2a0f5927d6c2d1faa7826e33d94293582c
Bryan Personal <bryanlohjw97@gmail.com>
2023-03-12 13:38:49 +0800

    style: auth page style

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -17,25 +17,25 @@ const Auth = () => {
   toggleHeight();
 
   return (
-    <div className="h-screen w-auto flex-col overflow-y-hidden bg-primary-600">
+    <div className="h-screen w-auto flex-col overflow-y-hidden bg-secondary">
       <div className={`relative ${height} p-5 transition-all duration-1000`}>
-        <h1 className="relative top-1/4 -translate-y-1/2 text-center text-4xl font-bold text-secondary">
+        <h1 className="relative top-1/4 -translate-y-1/2 text-center text-4xl font-bold text-primary">
           Dive Buddy
         </h1>
         <Image
           alt="dive buddy"
           src={diveBuddyIcon}
-          className="relative top-1/2 -translate-y-1/2"
+          className="relative top-2/3 left-1/2 w-72 -translate-y-1/2 -translate-x-1/2"
         />
       </div>
-      <div className="h-96 w-full rounded-3xl border-4 bg-secondary px-5 pt-10 text-primary-600">
+      <div className="h-96 w-full rounded-3xl bg-primary px-5 pt-10 text-secondary">
         <h1 className="mb-5 text-2xl font-bold">Welcome</h1>
         <p className="mb-5">
           Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit
           amet. Vitae tortor condimentum lacinia quis vel eros.
         </p>
         <button
-          className="w-48 border-2 border-solid border-secondary"
+          className="w-48"
           onClick={() => {
             void signIn("google", { callbackUrl: "http://localhost:3000/" });
           }}
@@ -43,6 +43,7 @@ const Auth = () => {
           <Image src={googleSignIn} alt="google icon" />
         </button>
       </div>
+      <div className="animation-test absolute">Hello</div>
     </div>
   );
 };
