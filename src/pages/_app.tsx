826322728f8714bafe39e4afcf53ec1533ae221a
Bryan Personal <bryanlohjw97@gmail.com>
2023-04-17 16:33:51 +0800

    feat: landing page fix + auto re-login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,10 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import "@fontsource/inter";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 import MyHead from "~/components/MyHead";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,6 +13,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Toaster />
       <MyHead />
       <Component {...pageProps} />
     </SessionProvider>
